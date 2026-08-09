84b3c61eddc66f2ef1e49cef1a3702e55dcf3a90
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-08-09 21:47:49 +0100

    Refactor shopping cart and drink menu functionality

--- OOP_practice.py
+++ OOP_practice.py
@@ -3,22 +3,19 @@ class Cart_item:
         self.model = model
         price = self.price
     
+    
+
 class Cart:
     def __init__(self,store,items):
         self.store = store
         self.items = items
         items = []    
-     
-
-    def store_name(self):
-        print(f"The store name is {self.store}")
 
 
     def shopping_cart_items(self):
         print(f"There are {len(self.items)} amount of items in the cart and they are; ",*self.items )
 
-    def add_item(self):
-        self.items.append()
+    
 
 
 def exit_program():
@@ -36,47 +33,55 @@ def exit_program():
 def get_details():
 
     while True:
-        model = input("\nEnter Model:")
 
-        
-        if len(model) < 1:
-            print("Please enter a valid item name!")
-        elif model.isdigit():
-            print("The item cannot be a number!")
+        try:
+            drink_no = int(input("\nEnter Drink number:"))
+        except ValueError:
+            print("The drink number is invalid!.")
+            continue
+
+        if drink_no not in range(1,10):
+            print("Your drink number is out of range!")
+
+        elif drink_no == 1:
+            pass
         else:
             break
+       
+        
+        
 
-    Store.items.append(model)
+    Shop.items.append(drink_no)
 
 
 
-Store = Cart("Amazon",[])
+Shop = Cart("Lloyds Coffee",[])
 
 
 
 def view_cart():
     
-    print(f"\n{Store.shopping_cart_items()}")
+    print(f"\n{Shop.shopping_cart_items()}")
 
 
 
 
 def remove_items():
 
-     print(*Store.items)
+     print(*Shop.items)
 
      while True:
-            item_to_remove = input("\nEnter item to remove:")
+            item_to_remove = input("\nEnter drink to remove:")
     
             if len(item_to_remove) < 1:
-                print("Please enter a valid item name!")
+                print("Please enter a valid drink name!")
             elif item_to_remove.isdigit():
-                print("The item cannot be a number!")
-            elif item_to_remove not in Store.items:
-                print("Item entered is not in your shopping cart!. Try again.")
+                print("The drink cannot be a number!")
+            elif item_to_remove not in Shop.items:
+                print("Drink entered is not in your cart!. Try again.")
             else:
-                Store.items.remove(item_to_remove)
-                print(f"{item_to_remove} has succesfully been removed from your shopping cart!.")
+                Shop.items.remove(item_to_remove)
+                print(f"{item_to_remove} has succesfully been removed from your drink cart!.")
                 view_cart()
                 break
     
@@ -84,17 +89,41 @@ def remove_items():
 
 
 def drink_menu():
+    drinks_menu = {
+    "1. Flat White": 3.40,
+    "2. Filter Coffee": 2.80,
+    "3. Iced Caramel Latte": 4.30,
+    "4. Cold Brew": 3.50,
+    "5. Hot Chocolate": 3.85,
+    "6. Chai Latte": 3.90,
+    "7. Matcha Latte": 4.20,
+    "8. English Breakfast Tea": 2.60,
+    "9. Peach Iced Tea": 3.75,
+    "10. Coffee Frappé": 4.50
+}
+    print(f"Welcome to {Shop.store}!")
+
     print("="* 30)
-    print
+
+    for drink, price in drinks_menu.items():
+        print(f"{drink} costs £{price:.2f}")
+
+    print("="* 30)
+
+    get_details()
+        
+
+    
 
 def main():
     flag = True
 
     print("#"*20)
-    print("\n## 1. Add Items")
-    print("## 2. View Cart")
-    print("## 3. Remove Items")
-    print("## 4. Exit Program")
+    print("\n## 1. View drink menu")
+    print("## 2. View added drinks")
+    print("## 3. Checkout")
+    print("## 4. Remove Items")
+    print("## 5. Exit Program")
     print("")
     print("#"*20)
 
@@ -109,25 +138,30 @@ def main():
             print("Invalid choice!.")
 
         elif choice == 1:
-            get_details()
+            drink_menu()
 
         elif choice == 2:
             view_cart()
 
         elif choice == 3:
-            if len(Store.items) == 0:
-                print("\nThere is no items in your cart to remove!.")
+            pass
+
+        elif choice == 4:
+            if len(Shop.items) == 0:
+                print("\nThere is no drinks in your cart to remove!.")
             else:
                 remove_items()
                 continue
             
-        elif choice == 4:
+        elif choice == 5:
             exit_program()
 
         else:
             break
 
-    
+      
+
+
 
 
 if __name__ == "__main__":
