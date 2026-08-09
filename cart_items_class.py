3cf96a18346da29f8310b410c77a86232469ba36
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-08-09 21:50:05 +0100

    Refactor Cart_item constructor to remove price

--- cart_items_class.py
+++ cart_items_class.py
@@ -1,12 +1,4 @@
-
 class Cart_item:
-    def __init__(self,model,price):
+    def __init__(self,model,):
         self.model = model
-        self.price = price
-    
-    def get_price(self):
-        return self.price
-    
-    def get_model(self):
-        return self.model
-    
+        price = self.price
