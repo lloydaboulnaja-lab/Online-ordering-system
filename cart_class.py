9a4666156676336096707b159026566c70b09e68
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-08-09 21:51:51 +0100

    Refactor cart_class by removing unused methods
    Removed store_name and add_item methods, updated shopping_cart_items method.

--- cart_class.py
+++ cart_class.py
@@ -3,13 +3,7 @@ class Cart:
         self.store = store
         self.items = items
         items = []    
-     
 
-    def store_name(self):
-        print(f"The store name is {self.store}")
 
     def shopping_cart_items(self):
-        print(f"There are {len(self.items)} amount of items in the cart and they are; {self.items}")
-
-    def add_item(self):
-        self.items.append()
+        print(f"There are {len(self.items)} amount of items in the cart and they are; ",*self.items )
